3a1eca75fa5c0ef2b234859402a78943e27ea29c
tuannguyenh1 <tuannguyenh1@nashtechglobal.com>
2024-12-24 22:30:39 +0700

    a

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -4,6 +4,8 @@ import "./App.scss";
 import AddTodo from "./components/AddTodo";
 import TodoList from "./components/TodoList";
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -15,7 +17,7 @@ export default class App extends React.Component {
 
   componentDidMount() {
     axios
-      .get("/api")
+      .get(`${API_URL}/api`)
       .then((response) => {
         this.setState({
           todos: response.data.data,
@@ -26,7 +28,7 @@ export default class App extends React.Component {
 
   handleAddTodo = (value) => {
     axios
-      .post("/api/todos", { text: value })
+      .post(`${API_URL}/api/todos`, { text: value })
       .then(() => {
         this.setState({
           todos: [...this.state.todos, { text: value }],
